be053c7cf5f873976253797135ae0d62aafb30e5
Jayrok0 <jkemm11@wgu.edu>
2025-08-09 19:04:29 -0400

    Finished data pipeline, dont even feel comfortable saying it for certain after that hurricane of a project

--- main.py
+++ main.py
@@ -135,5 +135,15 @@ def go(config: DictConfig):
         env = os.environ.copy()
         subprocess.run(command, check=True, env=env)
 
+    if "test_regression_model" in active_steps:
+        _ = mlflow.run(
+            config["main"]["components_repository"] + "/test_regression_model",
+            "main",
+            parameters={
+                "mlflow_model": "random_forest_export:prod",
+                "test_dataset": "test_data.csv:latest"
+            },
+        )
+
 if __name__ == "__main__":
     go()
